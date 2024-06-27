24bd80d7793321555520be8b60f2eab5819ea9d2
Anton <nanderson@banjohealth.com>
2024-06-27 17:59:17 +0200

    fix ui

--- src/Pages/ProfileCreate.tsx
+++ src/Pages/ProfileCreate.tsx
@@ -46,22 +46,22 @@ export const ProfileCreate: React.FC = () => {
 
     const handleSubmit=async (e: React.FormEvent, formData: any) => {
         e.preventDefault();
-        const token=localStorage.getItem('token');
+        const token = localStorage.getItem('token');
         try {
-            const response=await axios.post('http://localhost:4000/api/profile/new', {
-                avatar: formData.avatar,
-                phoneNumber: formData.phoneNumber,
-                gender: formData.gender,
-                address: {
-                    street: formData.address.street,
-                    street2: formData.address.street2,
-                    city: formData.address.city,
-                    country: formData.address.country,
-                    zipCode: formData.address.zipCode,
+            const response = await axios.post('http://localhost:4000/api/profile/new', {
+                    avatar: formData.avatar,
+                    phoneNumber: formData.phoneNumber,
+                    gender: formData.gender,
+                    address: {
+                        street: formData.address.street,
+                        street2: formData.address.street2,
+                        city: formData.address.city,
+                        country: formData.address.country,
+                        zipCode: formData.address.zipCode,
+                    },
+                    language: formData.language,
+                    timeZone: formData.timeZone,
                 },
-                language: formData.language,
-                timeZone: formData.timeZone,
-            },
                 {
                     headers: {
                         'Authorization': `Bearer ${token}`,
@@ -69,43 +69,14 @@ export const ProfileCreate: React.FC = () => {
                     }
                 });
             console.log(response);
-            if(response) {
+            if (response) {
                 dispatch(reqUsers);
                 navigate('/')
             }
+        } catch (e) {
 
-  const handleSubmit = async (e: React.FormEvent, formData: any) => {
-    e.preventDefault();
-    const token = localStorage.getItem("token");
-    try {
-      const response = await axios.post(
-        "http://localhost:4000/api/profile/new",
-        {
-          avatar: formData.avatar,
-          phoneNumber: formData.phoneNumber,
-          gender: formData.gender,
-          address: {
-            street: formData.address.street,
-            street2: formData.address.street2,
-            city: formData.address.city,
-            country: formData.address.country,
-            zipCode: formData.address.zipCode,
-          },
-          language: formData.language,
-          timeZone: formData.timeZone,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
-      );
-      console.log(response);
-    } catch (error) {
-      console.error("Error during updating profile:", error);
-      alert("An error occurred. Please try again.");
+        }
     }
-  };
 
   return (
     <div className="profileContainer">
@@ -238,91 +209,6 @@ export const ProfileCreate: React.FC = () => {
                 </div>
                 <button type="submit"> Save Changes </button>
             </form>
-
-        <div>
-          <label>Gender</label>
-          <select
-            name="gender"
-            value={formData.gender}
-            onChange={e => updateFormData(e)}
-            required>
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Non-binary</option>
-          </select>
-        </div>
-        <div>
-          <label>Street</label>
-          <input
-            type="text"
-            name="street"
-            value={formData.address.street}
-            onChange={e => updateFormData(e, true)}
-            required
-          />
-        </div>
-        <div>
-          <label>Street2</label>
-          <input
-            type="text"
-            name="street2"
-            value={formData.address.street2}
-            onChange={e => updateFormData(e, true)}
-            required
-          />
-        </div>
-        <div>
-          <label>City</label>
-          <input
-            type="text"
-            name="city"
-            value={formData.address.city}
-            onChange={e => updateFormData(e, true)}
-            required
-          />
-        </div>
-        <div>
-          <label>Country</label>
-          <input
-            type="text"
-            name="country"
-            value={formData.address.country}
-            onChange={e => updateFormData(e, true)}
-            required
-          />
-        </div>
-        <div>
-          <label>Zip code</label>
-          <input
-            type="text"
-            name="zipCode"
-            value={formData.address.zipCode}
-            onChange={e => updateFormData(e, true)}
-            required
-          />
-        </div>
-        <div>
-          <label>Language</label>
-          <input
-            type="text"
-            name="language"
-            value={formData.language}
-            onChange={e => updateFormData(e)}
-            required
-          />
-        </div>
-        <div>
-          <label>Time zone</label>
-          <input
-            type="text"
-            name="timeZone"
-            value={formData.timeZone}
-            onChange={e => updateFormData(e)}
-            required
-          />
-        </div>
-        <button type="submit"> Save Changes </button>
-      </form>
     </div>
   );
 };
