72ee4ccfc9cfb1d9ffea5cd8b130c0a62afbf4bb
Qinastha <146641022+Qinastha@users.noreply.github.com>
2024-07-17 20:26:25 +0300

    minor changes

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -1,117 +1,118 @@
-import { NavLink, Outlet, useLocation, useNavigate } from "react-router-dom";
+import {NavLink, Outlet, useLocation, useNavigate} from "react-router-dom";
 import "./Layout.scss";
-import React, { useState } from "react";
+import React, {useState} from "react";
 import pinkBlossom from "../../assets/pinkBlossom.png";
-import { useAppDispatch } from "../../hooks";
-import { setIsNewProject, setProjectOpen } from "../../store/projectSlice";
+import {useAppDispatch} from "../../hooks";
+import {setIsNewProject, setProjectOpen} from "../../store/projectSlice";
 import PopUp from "../PopUp/PopUp";
-import { setUserInitial } from "../../store/userSlice";
+import {setProfileNull, setUserInitial} from "../../store/userSlice";
 
 export const Layout: React.FC = () => {
-  const { pathname } = useLocation();
-  const [isExpand, setIsExpand] = useState(true);
-  const [isOpen, setIsOpen] = useState(false);
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
+    const {pathname} = useLocation();
+    const [isExpand, setIsExpand] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
+    const navigate = useNavigate();
+    const dispatch = useAppDispatch();
 
-  const toggleNav = (): void => {
-    setIsExpand(!isExpand);
-  };
+    const toggleNav = (): void => {
+        setIsExpand(!isExpand);
+    };
 
-  const handleOpen = (): void => {
-    dispatch(setProjectOpen(true));
-    dispatch(setIsNewProject(true));
-  };
+    const handleOpen = (): void => {
+        dispatch(setProjectOpen(true));
+        dispatch(setIsNewProject(true));
+    };
 
-  const handleLogout = (): void => {
-    localStorage.removeItem("token");
-    navigate("/login");
-    dispatch(setUserInitial(true));
-  };
+    const handleLogout = (): void => {
+        localStorage.removeItem("token");
+        navigate("/login");
+        dispatch(setUserInitial(true));
+        dispatch(setProfileNull())
+    };
 
-  const getPageTitle = (pathname: string): string => {
-    switch (pathname) {
-      case "/":
-        return "Dashboard";
-      case "/projects":
-        return "Projects";
-      case "/tasks":
-        return "Tasks";
-      case "/logs":
-        return "Time Log";
-      case "/resources":
-        return "Resource Mgnt";
-      case "/users":
-        return "User";
-      case "/templates":
-        return "Project Template";
-      case "/settings/app":
-        return "Global Settings";
-      case "/settings/profile":
-        return "Profile Settings";
-      default:
-        return "Unknown";
-    }
-  };
+    const getPageTitle = (pathname: string): string => {
+        switch (pathname) {
+            case "/":
+                return "Dashboard";
+            case "/projects":
+                return "Projects";
+            case "/tasks":
+                return "Tasks";
+            case "/logs":
+                return "Time Log";
+            case "/resources":
+                return "Resource Mgnt";
+            case "/users":
+                return "User";
+            case "/templates":
+                return "Project Template";
+            case "/settings/app":
+                return "Global Settings";
+            case "/settings/profile":
+                return "Profile Settings";
+            default:
+                return "Unknown";
+        }
+    };
 
-  return (
-    <div className="container">
-      <header className={`navbar ${isExpand ? "" : "notExpanded"}`}>
-        <div className="navButtons">
-          <button className="collapseButton" type="button" onClick={toggleNav}>
-            &#8656;
-          </button>
-          <button
-            className="newProjectButton"
-            type="button"
-            onClick={handleOpen}>
-            Create New Project
-          </button>
+    return (
+        <div className="container">
+            <header className={`navbar ${isExpand ? "" : "notExpanded"}`}>
+                <div className="navButtons">
+                    <button className="collapseButton" type="button" onClick={toggleNav}>
+                        &#8656;
+                    </button>
+                    <button
+                        className="newProjectButton"
+                        type="button"
+                        onClick={handleOpen}>
+                        Create New Project
+                    </button>
+                </div>
+
+                <div>
+                    <PopUp/>
+                </div>
+
+                <NavLink to="/"> Dashboard </NavLink>
+                <NavLink to="/projects"> Projects </NavLink>
+                <NavLink to="/tasks"> Tasks </NavLink>
+                <NavLink to="/logs"> Time Log </NavLink>
+                <NavLink to="/resources"> Resource Mgnt </NavLink>
+                <NavLink to="/users"> User </NavLink>
+                <NavLink to="/templates"> Project Template </NavLink>
+            </header>
+
+            <main>
+                <div className="coreContent">
+                    <div className="heading">
+                        <h1>{getPageTitle(pathname)}</h1>
+                        <div className="headingRightPart">
+                            <input type="text" placeholder="Search for anything..."/>
+                            <div className="dropdown-container">
+                                <img
+                                    className="settingIcon"
+                                    src={pinkBlossom}
+                                    alt="Settings"
+                                    onClick={(): void => setIsOpen(!isOpen)}
+                                />
+                                {isOpen && (
+                                    <div className="dropdown-menu">
+                                        <NavLink to="/settings/app"> Global Settings</NavLink>
+                                        <NavLink to="/settings/profile"> Profile Settings</NavLink>
+                                    </div>
+                                )}
+                            </div>
+                            <button className="logoutButton" onClick={handleLogout}>
+                                LogOut
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="outlet">
+                    <Outlet/>
+                </div>
+            </main>
         </div>
-
-        <div>
-          <PopUp />
-        </div>
-
-        <NavLink to="/"> Dashboard </NavLink>
-        <NavLink to="/projects"> Projects </NavLink>
-        <NavLink to="/tasks"> Tasks </NavLink>
-        <NavLink to="/logs"> Time Log </NavLink>
-        <NavLink to="/resources"> Resource Mgnt </NavLink>
-        <NavLink to="/users"> User </NavLink>
-        <NavLink to="/templates"> Project Template </NavLink>
-      </header>
-
-      <main>
-        <div className="coreContent">
-          <div className="heading">
-            <h1>{getPageTitle(pathname)}</h1>
-            <div className="headingRightPart">
-              <input type="text" placeholder="Search for anything..." />
-              <div className="dropdown-container">
-                <img
-                  className="settingIcon"
-                  src={pinkBlossom}
-                  alt="Settings"
-                  onClick={(): void => setIsOpen(!isOpen)}
-                />
-                {isOpen && (
-                  <div className="dropdown-menu">
-                    <NavLink to="/settings/app"> Global Settings</NavLink>
-                    <NavLink to="/settings/profile"> Profile Settings</NavLink>
-                  </div>
-                )}
-              </div>
-              <button className="logoutButton" onClick={handleLogout}>
-                LogOut
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="outlet">
-          <Outlet />
-        </div>
-      </main>
-    </div>
-  );
+    );
 };
--- src/core/constants/popUpInputs.constants.ts
+++ src/core/constants/popUpInputs.constants.ts
@@ -1,27 +1,27 @@
-import { RequiredInput } from "../interfaces/requireInput";
+import {RequiredInput} from "../interfaces/requireInput";
 
 export const POPUP_REQUIRED_INPUTS: RequiredInput[] = [
-  {
-    type: "text",
-    name: "projectName",
-    label: "Project name",
-    required: true,
-    className: "form-control",
-    autoComplete: "off",
-  },
-  {
-    type: "text",
-    name: "projectDescription",
-    label: "Description",
-    required: true,
-    className: "form-control",
-    autoComplete: "off",
-  },
-  {
-    type: "file",
-    name: "projectAvatar",
-    className: "form-control",
-    required: true,
-    label: "Project Picture",
-  },
+    {
+        type: "text",
+        name: "projectName",
+        label: "Project name",
+        required: true,
+        className: "form-control",
+        autoComplete: "off",
+    },
+    {
+        type: "text",
+        name: "projectDescription",
+        label: "Description",
+        required: true,
+        className: "form-control",
+        autoComplete: "off",
+    },
+    {
+        type: "file",
+        name: "projectAvatar",
+        className: "form-control",
+        required: true,
+        label: "Project Picture",
+    },
 ];
