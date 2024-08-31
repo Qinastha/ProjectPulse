1fa17f43d939918d7e96c3138036792d45552727
Qinastha <146641022+Qinastha@users.noreply.github.com>
2024-08-31 19:32:51 +0300

    Requests updated

--- src/core/interceptors/authInterceptor.ts
+++ src/core/interceptors/authInterceptor.ts
@@ -1,67 +1,67 @@
-import axios, { AxiosRequestHeaders } from "axios";
-import { useEffect } from "react";
-import { useAppDispatch } from "../../hooks";
-import { setAlert } from "../../store/alertSlice";
+import axios, {AxiosRequestHeaders} from "axios";
+import {useEffect} from "react";
+import {useAppDispatch} from "../../hooks";
+import {setAlert} from "../../store/alertSlice";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000/api/",
-  timeout: 100000,
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: "http://51.21.127.157:4000/api/",
+    timeout: 100000,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 axiosInstance.interceptors.request.use(
-  config => {
-    const token = localStorage.getItem("token") || "";
-    const location = config.url;
+    config => {
+        const token = localStorage.getItem("token") || "";
+        const location = config.url;
 
-    config.headers = {
-      Accept: "application/json",
-    } as AxiosRequestHeaders;
+        config.headers = {
+            Accept: "application/json",
+        } as AxiosRequestHeaders;
 
-    if (location !== "auth/login" && location !== "auth/register") {
-      config.headers = {
-        ...config.headers,
-        Authorization: `Bearer ${token}`,
-      } as AxiosRequestHeaders;
-    }
+        if (location !== "auth/login" && location !== "auth/register") {
+            config.headers = {
+                ...config.headers,
+                Authorization: `Bearer ${token}`,
+            } as AxiosRequestHeaders;
+        }
 
-    return config;
-  },
-  error => Promise.reject(error),
+        return config;
+    },
+    error => Promise.reject(error),
 );
 
 interface AxiosInterceptorProps {
-  children: any;
+    children: any;
 }
 
-function AxiosInterceptor({ children }: AxiosInterceptorProps) {
-  const dispatch = useAppDispatch();
+function AxiosInterceptor({children}: AxiosInterceptorProps) {
+    const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    const resInterceptor = (response: any) => {
-      dispatch(setAlert(response.data.alert));
-      return response;
-    };
+    useEffect(() => {
+        const resInterceptor = (response: any) => {
+            dispatch(setAlert(response.data.alert));
+            return response;
+        };
 
-    const errInterceptor = (error: any) => {
-      console.log(error);
-      dispatch(setAlert(error.response.data.alert));
-      return Promise.reject(error);
-    };
+        const errInterceptor = (error: any) => {
+            console.log(error);
+            dispatch(setAlert(error.response.data.alert));
+            return Promise.reject(error);
+        };
 
-    const interceptor = axiosInstance.interceptors.response.use(
-      resInterceptor,
-      errInterceptor,
-    );
+        const interceptor = axiosInstance.interceptors.response.use(
+            resInterceptor,
+            errInterceptor,
+        );
 
-    return () => axiosInstance.interceptors.response.eject(interceptor);
-  }, []);
+        return () => axiosInstance.interceptors.response.eject(interceptor);
+    }, []);
 
-  return children;
+    return children;
 }
 
 export default axiosInstance;
 
-export { AxiosInterceptor };
+export {AxiosInterceptor};
